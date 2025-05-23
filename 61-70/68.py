5e8e1e562bbc40f3b26c6379cbca246ed4cace60
AsRumi <mohammedmutahar3@gmail.com>
2025-05-23 11:58:46 +0530

    Updated answer for 68.py

--- 61-70/68.py
+++ 61-70/68.py
@@ -9,8 +9,8 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
-    
     def createList(self, list):
         if not list:
             return None
@@ -21,12 +21,37 @@ class Solution:
             current = current.next
         return head
     
+    def printList(self, head):
+        current = head
+        while(current.next):
+            print(current.val)
+            current = current.next
+        print(current.val)
+        return head
+    
     def reverseList(self, head):
         head = self.createList(head)
-        current = head
-        
-        return None
+        # head = self.printList(head)
+        one = head
+        if not one:
+            return None
+        two = one.next
+        if not two:
+            return head
+        three = two.next
+        if not three:
+            one.next = None
+            two.next = one
+            return two
+        one.next = None
+        while(three):
+            two.next = one
+            one = two
+            two = three
+            three = three.next
+        two.next = one
+        return two
     
 answer = Solution()
-reversed_list = answer.reverseList(head = [1,2,3,4,5])
+reversed_list = answer.reverseList(head = [1, 2, 3, 4, 5])
 print(f"Reversed List: {reversed_list}")
