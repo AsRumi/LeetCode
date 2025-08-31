53a88ec960250a32fc1dccebd40680006834f945
AsRumi <mohammedmutahar3@gmail.com>
2025-08-30 22:10:58 -0400

    feat: Brute force and optimized solution worked out for LeetCode 90, took me 24+ hours to come up with an optimized solution :(

--- 81-90/85.py
+++ 81-90/85.py
@@ -5,5 +5,44 @@ from typing import List
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res, sol = [], []
-        
+        nums.sort()
+        l = len(nums)
+        def backtrack(start):
+            if start == l:
+                res.append(sol[:])
+                return
+            else:
+                res.append(sol[:])
+            i = start
+            while(i < l):
+                if i > start and nums[i] == nums[i-1]:
+                    i += 1
+                    continue
+                sol.append(nums[i])
+                backtrack(i+1)
+                sol.pop()
+                i += 1
+        backtrack(0)
         return res
+    
+    def subsetsWithDupBF(self, nums: List[int]) -> List[List[int]]:
+        res, sol = [], []
+        l = len(nums)
+        nums.sort(reverse=True)
+        def backtrack(i):
+            if i == l:
+                if sol not in res:
+                    res.append(sol[:])
+                    return
+                else:
+                    return
+            backtrack(i+1)
+            sol.append(nums[i])
+            backtrack(i+1)
+            sol.pop()
+        backtrack(0)
+        return res
+    
+ans = Solution()
+subsets = ans.subsetsWithDup(nums = [1, 1, 2, 3, 3])
+print(f"Subsets without duplication: {subsets}")
