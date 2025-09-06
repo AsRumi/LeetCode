6250a587e03506c24927f21cf33a8c1eb750973c
AsRumi <mohammedmutahar3@gmail.com>
2025-09-06 18:16:08 -0400

    feat: attempted 3 questions, 2 from contests.

---
+++ Weekly Contest 464/2.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> bool:
+        
+        return True
+    
+ans = Solution()
+partitionArray = ans.partitionArray(nums = [1,2,3,4], k = 2)
+print(f"Partition Array: {partitionArray}")
